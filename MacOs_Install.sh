5030e8782f51323d47439712afe7183cde72cf86
srvaldomirooliveira <mr.oliveiravaldomiro@gmail.com>
2021-11-19 15:05:02 -0300

    master

---
+++ MacOs_Install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo apt update -y && apt upgrade -y
+
+sudo apt install gtk2-engines-murrine gtk2-engines-pixbuf tweak gnome-tweaks -y
+
+echo "\n Extract Themes \n"
+
+cd Themes\
+
+for arq in *.tar.xz; do
+
+	tar xf $arq;
+
+done
+
+rm *.tar.xz
+
+mv * /usr/share/themes/
+
+echo "\n Extract Finish \n"
+
+
+cd ../Icons/
+
+echo "\n Extract Icons \n"
+
+for arq in *.tar.xz; do
+
+	tar xf $arq;
+
+done
+
+rm *.tar.xz
+
+for arq in *.zip; do
+
+	unzip -qq "$arq";
+
+done
+
+rm *.zip
+
+mv * /usr/share/themes/
+
+echo "\n Extract Finish \n"
